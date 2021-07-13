08c875ae47641e0e6b562b4150097ec602812514
esti-elish <212042279@mby.co.il>
2021-07-13 18:02:26 +0300

    try

--- makeupServer/Controllers/ValuesController.cs
+++ makeupServer/Controllers/ValuesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,11 +22,28 @@ namespace makeupServer.Controllers
         }
 
         // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{email}")]
+        public string Get(string email)
+
         {
-            return "value";
+            WebRequest request;
+            request = WebRequest.Create("http://127.0.0.1:9007/sentiment/?email=" + email);
+            WebResponse response = request.GetResponse();
+            string responseFromServer = string.Empty;
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+            }
+            //טיפול בתשובה שחזרה מהשרת
+            response.Close();
+            return responseFromServer;
         }
+
 
         // POST api/<ValuesController>
         [HttpPost]
